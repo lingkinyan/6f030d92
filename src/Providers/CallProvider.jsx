f1fd339a4cdb7679232f71e5c159a36103f877c4
lingkinyan <kyling21@gmail.com>
2024-07-12 19:02:54 -0400

    feat: logic to fetch all calls

---
+++ src/Providers/CallProvider.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState, createContext } from "react";
+import useSWR, { useSWRConfig } from "swr";
+import axios from "axios";
+
+import { BASE_URL, GET_CALLS, RESET } from "../endpoints.js";
+
+const fetcher = async (url) => {
+  const res = await axios.get(url);
+  return res.data;
+};
+
+export const CallContext = createContext({
+  data: [],
+  isLoading: false,
+});
+
+function CallProvider({ children }) {
+  const [info, setInfo] = useState({
+    data: [],
+    isLoading: false,
+  });
+
+  const { data, isLoading } = useSWR(
+    BASE_URL.concat(GET_CALLS),
+    fetcher
+  );
+
+  useEffect(() => {
+    if (data) setInfo({ data, isLoading });
+    else setInfo({ data: [], isLoading: false });
+  }, [data]);
+
+  return <CallContext.Provider value={info}>{children}</CallContext.Provider>;
+}
+
+export default CallProvider;
