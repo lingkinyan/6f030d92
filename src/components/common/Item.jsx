b71c18c29e29b1b2a8243c7b457a9c6f17b1b04f
lingkinyan <kyling21@gmail.com>
2024-07-13 12:06:32 -0400

    fix: removed empty line

--- src/components/common/Item.jsx
+++ src/components/common/Item.jsx
@@ -1,7 +1,6 @@
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
 
-
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
