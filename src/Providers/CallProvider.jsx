d6a6c3662fafa7dbd8cc0d5571082986932a18bd
lingkinyan <kyling21@gmail.com>
2024-07-13 11:15:16 -0400

    feat: context to cover all call data and tab change event

--- src/Providers/CallProvider.jsx
+++ src/Providers/CallProvider.jsx
@@ -20,13 +20,85 @@ function CallProvider({ children }) {
     isLoading: false,
   });
 
-  const { data, isLoading } = useSWR(
+  const { data, isLoading, isValidating } = useSWR(
     BASE_URL.concat(GET_CALLS),
     fetcher
   );
 
+  console.log(isValidating)
   useEffect(() => {
-    if (data) setInfo({ data, isLoading });
+    // setInfo({
+    //   data:[
+    //     {
+    //       "direction": "inbound",
+    //       "from": 1,
+    //       "to": 2,
+    //       "via": 1,
+    //       "duration": 0,
+    //       "is_archived": true,
+    //       "call_type": "answered",
+    //       "id": "6685a0df24a7a79ae0c50f8f",
+    //       "created_at": "2024-07-03T05:05:03.506Z"
+    //     },
+    //     {
+    //       "direction": "inbound",
+    //       "from": 1,
+    //       "to": 2,
+    //       "via": 1,
+    //       "duration": 0,
+    //       "is_archived": true,
+    //       "call_type": "answered",
+    //       "id": "6685a0df24a7a79ae0c50f8f",
+    //       "created_at": "2024-07-03T23:05:03.506Z"
+    //     },
+    //     {
+    //       "direction": "inbound",
+    //       "from": 1,
+    //       "to": 2,
+    //       "via": 1,
+    //       "duration": 0,
+    //       "is_archived": true,
+    //       "call_type": "answered",
+    //       "id": "6685a0df24a7a79ae0c50f8f",
+    //       "created_at": "2024-07-03T19:05:03.506Z"
+    //     },
+    //     {
+    //       "direction": "outbound",
+    //       "from": 2,
+    //       "to": 1,
+    //       "via": 1,
+    //       "duration": 0,
+    //       "is_archived": true,
+    //       "call_type": "answered",
+    //       "id": "6685b79524326ad725d48041",
+    //       "created_at": "2024-07-03T20:41:57.436Z"
+    //     },
+    //     {
+    //       "direction": "outbound",
+    //       "from": 2,
+    //       "to": 1,
+    //       "via": 1,
+    //       "duration": 0,
+    //       "is_archived": true,
+    //       "call_type": "answered",
+    //       "id": "6685b79524326ad725d48041",
+    //       "created_at": "2024-07-04T20:41:57.436Z"
+    //     },
+    //     {
+    //       "direction": "outbound",
+    //       "from": 2,
+    //       "to": 1,
+    //       "via": 1,
+    //       "duration": 0,
+    //       "is_archived": true,
+    //       "call_type": "answered",
+    //       "id": "6685b79524326ad725d48041",
+    //       "created_at": "2024-07-04T20:41:57.436Z"
+    //     }
+    //   ],
+    //   isLoading: false,
+    // })
+    if (data) setInfo({ data, isLoading: isLoading && isValidating });
     else setInfo({ data: [], isLoading: false });
   }, [data]);
 
---
+++ src/Providers/TabProvider.jsx
@@ -0,0 +1,21 @@
+import React, { useState, createContext } from "react";
+import { Tabs } from '../constants/tabs'
+
+export const TabContext = createContext({
+    currentTab: Object.keys(Tabs)[0],
+    setCurrentTab: () => {},
+});
+
+function TabProvider({ children }) {
+    const [tab, setTab] = useState(Object.keys(Tabs)[0]);    
+    return (
+        <TabContext.Provider value={{
+            currentTab: tab,
+            setCurrentTab: setTab
+        }}>
+            {children}
+        </TabContext.Provider>
+    )
+}
+
+export default TabProvider;
